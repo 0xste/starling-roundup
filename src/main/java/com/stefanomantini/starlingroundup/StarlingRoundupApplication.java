dcd691bfd98bf92240141d76b54a4b688f192dfc
Stefano <sman6798@gmail.com>
2020-07-04 17:46:44 +0100

    fix: added google java format to fix failing pipeline

--- src/main/java/com/stefanomantini/starlingroundup/StarlingRoundupApplication.java
+++ src/main/java/com/stefanomantini/starlingroundup/StarlingRoundupApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StarlingRoundupApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StarlingRoundupApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(StarlingRoundupApplication.class, args);
+  }
 }
